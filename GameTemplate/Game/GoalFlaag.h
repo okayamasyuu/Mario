3a0c2c9a7f107af66614d7ead4224bc1e98e7418
kbc18b06@stu.kawahara.ac.jp <1>
2019-06-17 10:55:49 +0900

    クラスの追加 リザルト画面とシーンの切り替え

--- GameTemplate/Game/GoalFlaag.h
+++ GameTemplate/Game/GoalFlaag.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Game.h"
 class Game;
 class GoalFlaag : public IGameObject
 {
@@ -7,10 +8,20 @@ public:
 	~GoalFlaag();
 	bool Start();
 	void Update();
+	void SetClear_flag(bool flag)
+	{
+		Clear_flag = flag;
+	}
+	bool GetClearFlag() 
+	{
+		return Clear_flag;
+	}
 private:
 	prefab::CSkinModelRender* m_goalflaagModel = nullptr;
+	CPhysicsStaticObject m_PSOject;
 	CVector3 m_position = CVector3::Zero;
 	CVector3 scela;
+	bool Clear_flag = false; //クリアフラグ
 	Game* ga = nullptr;
 };
 
---
+++ GameTemplate/Game/Result.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "Result.h"
+#include "title.h"
+
+Result::Result()
+{
+}
+
+
+Result::~Result()
+{
+	DeleteGO(m_resultSprite);
+}
+bool Result::Start()
+{
+	m_resultSprite = NewGO<prefab::CSpriteRender>(0);
+	m_resultSprite->Init(L"sprite/banana.dds", 500, 200);//ƒŠƒUƒ‹ƒg‰æ–Ê
+	return true;
+}
+void Result::Update()
+{
+	if (Pad(0).IsPress(enButtonA) == true) {
+		NewGO<title>(0);
+		DeleteGO(this);
+	}
+}
