e2c60e199776997d5531d3f5a65c0fb78f36486f
kbc18b06@stu.kawahara.ac.jp <1>
2019-06-18 12:52:51 +0900

    カメラクラス

--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -27,11 +27,21 @@ bool Player::Start()
 }
 void Player::Update()
 {
-	CPad& pad = Pad(0);
+	camera = FindGO<Camera>("ƒJƒƒ‰");
+
+	//CVector3 cameraForward = camera->GetCameraTargetPos() - camera->GetcamePos();
+	//cameraForward.y = 0.0f;
+	//cameraForward.Normalize();
+	//m_position += cameraForward * 5.0f * pad.GetLStickYF();
+
+	//CVector3 cameraRight;
+	//cameraRight.Cross({ 0.0f, 1.0f, 0.0f }, cameraForward);
+	//cameraRight.Normalize();
+	//m_position += cameraRight * 5.0f * pad.GetLStickXF();
+	
+
 	m_moveSpeed.x = pad.GetLStickXF() * 750.0f;
 	m_moveSpeed.z = pad.GetLStickYF() * 750.0f;
-
-	camera = FindGO<Camera>("ƒJƒƒ‰");
 
 	/*CVector3 cameraDir = 
 		camera->GetCameraTargetPos() - camera->GetcamePos();
